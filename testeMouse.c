fe904233b3548f22a1a9b1f5ac1ca0f7641310a7
Lucas Gabriel <lucasfacul20222@gmail.com>
2024-06-30 23:48:24 -0300

    feat: Testando colisão entre sprites e blocos

--- testeMouse.c
+++ testeMouse.c
@@ -23,13 +23,15 @@ int main() {
         return 1;
     }
 
+    set_cor_background_wbr(7, 7, 7);
+
     int x_real = 320, y_real = 240;
     
-    int x_mouse, y_mouse;
-
     int x_sprite = 400, y_sprite = 300;
+    int bloco_x = 200, bloco_y = 150;
 
-    set_sprite_wbr(1, x_sprite, y_sprite, 1, 2);
+    set_sprite_wbr(1, x_sprite, y_sprite, 30, 2);
+    edit_bloco_background_wbm(bloco_x, bloco_y, 0, 0, 0);
 
     // Loop infinito para receber as coordenadas do mouse
     while (1) {
@@ -48,13 +50,11 @@ int main() {
 
         if (ev.type == EV_REL) {
             if (ev.code == REL_X) {
-                x_mouse = ev.value;
-                x_real += x_mouse;
+                x_real += ev.value;
             } 
             
             else if (ev.code == REL_Y) {
-                y_mouse = ev.value;
-                y_real += y_mouse;
+                y_real += ev.value;
             }
         }
 
@@ -67,27 +67,31 @@ int main() {
         
         //Limitar as coordenadas acumuladas
         if (x_real < 0) x_real = 0;
-
         if (x_real > 619) x_real = 619;
-
         if (y_real < 0) y_real = 0;
-
         if (y_real > 459) y_real = 459;
-        
-        set_sprite_wbr(1, x_real, y_real, 0, 1);
 
-        //COLISÃO
-        if (x_real < x_sprite + 22 && 
-            x_real + 22 > x_sprite && 
-            y_real < y_sprite + 23 && 
-            y_real + 23 > y_sprite) {
+        //COLISÃO POLICIA
+        if (x_real + 5 <= x_sprite + 16 &&        //esquerda
+            x_real + 15 >= x_sprite + 3 &&        //direita
+            y_real + 1 <= y_sprite + 18 &&        //cima
+            y_real + 18 >= y_sprite + 1) {        //baixo
                 
             printf("COLISÃO\n");
-            break;
         }
 
+        /*
+        //COLISÃO BLOCO
+        if (x_real + 5 <= bloco_x + 8 &&        //esquerda
+            x_real + 15 >= bloco_x &&           //direita
+            y_real + 1 <= bloco_y + 8 &&        //cima
+            y_real + 18 >= bloco_y) {           //baixo
+                
+            printf("COLISÃO\n");
+        }*/
+        
         else{
-            printf("NÃO TEVE COLISÃO\n");
+            set_sprite_wbr(1, x_real, y_real, 0, 1);
         }
     }
 
