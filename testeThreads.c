bfbd0e912014c59f7948e8385f042bfb86b30096
Lucas Gabriel <142092072+LucaasGy@users.noreply.github.com>
2024-07-03 14:26:23 -0300

    feat: Mouse fica na thread

--- testeThreads.c
+++ testeThreads.c
@@ -8,21 +8,111 @@
 
 #define MOUSE_DEVICE "/dev/input/event0"
 
-volatile int x_sprite = 0;
-volatile int colisao = 0;
+volatile int x_real = 0;
+volatile int y_real = 0;
+volatile int clicou = 0;
+volatile int parar = 0;
 
-pthread_mutex_t x_sprite_mutex; 
+int fd_mouse;
+struct input_event ev;
 
-void* movimenta_sprite(void* arg) {
-    int sentido = 1;
+pthread_mutex_t lock; 
+
+void* movimenta_mouse(void* arg) {
+    ssize_t n;
+    int parou2;
     
-    while(colisao!=1){
-        //7 segundos / 619 (7 000 000 dividido pelo total de pixels) -> Tempo para um pixel ir de 0 a 619
-        usleep(11300);
+    while(1){
+        n = read(fd_mouse, &ev, sizeof(ev));
+            
+        if (n == (ssize_t)-1) {
+            fprintf(stderr, "Erro de leitura\n");
+            break;
+        } 
         
-        if (colisao) break;
+        if (n != sizeof(ev)) {
+            fprintf(stderr, "Erro ao ler %ld bytes, o esperado era %ld\n", n, sizeof(ev));
+            break;
+        }
 
-        pthread_mutex_lock(&x_sprite_mutex);
+        pthread_mutex_lock(&lock);
+
+        if (ev.type == EV_REL) {
+            if (ev.code == REL_X) {
+                x_real += ev.value;
+            } 
+            
+            else if (ev.code == REL_Y) {
+                y_real += ev.value;
+            }
+        }
+        
+        //Limitar as coordenadas acumuladas
+        if (x_real < 0) x_real = 0;
+        if (x_real > 619) x_real = 619;
+        if (y_real < 0) y_real = 0;
+        if (y_real > 459) y_real = 459;
+
+        set_sprite_wbr(1, x_real, y_real, 0, 1);
+
+        pthread_mutex_unlock(&lock);
+
+        if (ev.type == EV_KEY && ev.code == BTN_LEFT) {
+            if (ev.value == 1) {
+                clicou = 1;
+                break;
+            } 
+        }
+
+        pthread_mutex_lock(&lock);
+        parou2 = parar;
+        pthread_mutex_unlock(&lock);
+
+        if(parou2) break;
+    }
+    
+    //FINALIZA A EXECUÇÃO DA THREAD
+    pthread_exit(NULL); // Finaliza a thread
+}
+
+int main() {
+    pthread_t thread_id;
+    int x_sprite = 0;
+
+    //Abre arquivo de comunicação com o driver
+    if (open_driver( ) == -1){
+        return -1;
+    }
+
+    // Abrir o dispositivo do mouse
+    fd_mouse = open(MOUSE_DEVICE, O_RDONLY);
+    if (fd_mouse == -1) {
+        fprintf(stderr, "Erro ao abrir o mouse\n");
+        return 1;
+    }
+
+    set_cor_background_wbr(7,7,7);
+
+    //Mouse
+    set_sprite_wbr(1, x_real, y_real, 0, 1);
+
+    //Bixo correndo
+    set_sprite_wbr(1, x_sprite, 240, 0, 2);
+
+    // Inicializa o mutex de sprite
+    pthread_mutex_init(&lock, NULL);
+    
+    if (pthread_create(&thread_id, NULL, movimenta_mouse, NULL) != 0) {
+        perror("pthread_create");
+        return 1;
+    }
+
+    int sentido = 1;
+    int temp_x_real, temp_y_real, clicou2;
+    
+    while(1){
+        //7 segundos / 619 (7 000 000 dividido pelo total de pixels) -> Tempo para um pixel ir de 0 a 619
+        usleep(10000);
 
         //Desloca da esquerda para direita
         if(sentido == 1){
@@ -39,108 +129,34 @@ void* movimenta_sprite(void* arg) {
                 sentido = 1;
             }
         }
-
-        pthread_mutex_unlock(&x_sprite_mutex);
         
-        set_sprite_wbr(1, x_sprite, 240, 1, 2);
-    }
-    
-    //FINALIZA A EXECUÇÃO DA THREAD
-    pthread_exit(NULL); // Finaliza a thread
-}
+        set_sprite_wbr(1, x_sprite, 224, 1, 2);
 
-int main() {
-    int fd_mouse;
-    struct input_event ev;
-    ssize_t n;
-    pthread_t thread_id;
-
-    //Abre arquivo de comunicação com o driver
-    if (open_driver( ) == -1){
-        return -1;
-    }
-
-    // Abrir o dispositivo do mouse
-    fd_mouse = open(MOUSE_DEVICE, O_RDONLY);
-    if (fd_mouse == -1) {
-        fprintf(stderr, "Erro ao abrir o mouse\n");
-        return 1;
-    }
-
-    int x_real = 0, y_real = 0;
-
-    //Mouse
-    set_sprite_wbr(1, x_real, y_real, 0, 1);
-
-    //Bixo correndo
-    set_sprite_wbr(1, x_sprite, 240, 0, 2);
-
-    // Inicializa o mutex de sprite
-    pthread_mutex_init(&x_sprite_mutex, NULL);
-    
-    if (pthread_create(&thread_id, NULL, movimenta_sprite, NULL) != 0) {
-        perror("pthread_create");
-        return 1;
-    }
-
-    // Loop infinito para receber as coordenadas do mouse
-    while (1) {
-        // Ler o evento do mouse
-        n = read(fd_mouse, &ev, sizeof(ev));
-        
-        if (n == (ssize_t)-1) {
-            fprintf(stderr, "Erro de leitura\n");
-            break;
-        } 
-        
-        if (n != sizeof(ev)) {
-            fprintf(stderr, "Erro ao ler %ld bytes, o esperado era %ld\n", n, sizeof(ev));
-            break;
-        }
-
-        if (ev.type == EV_REL) {
-            if (ev.code == REL_X) {
-                x_real += ev.value;
-            } 
-            
-            else if (ev.code == REL_Y) {
-                y_real += ev.value;
-            }
-        }
-
-        if (ev.type == EV_KEY && ev.code == BTN_LEFT) {
-            if (ev.value == 1) {
-                printf("Botão esquerdo pressionado\n");
-                break;
-            } 
-        }
-        
-        //Limitar as coordenadas acumuladas
-        if (x_real < 0) x_real = 0;
-        if (x_real > 619) x_real = 619;
-        if (y_real < 0) y_real = 0;
-        if (y_real > 459) y_real = 459;
-
-        set_sprite_wbr(1, x_real, y_real, 0, 1);
-    
-        pthread_mutex_lock(&x_sprite_mutex);
-        int temp_x_sprite = x_sprite;
-        pthread_mutex_unlock(&x_sprite_mutex);
+        pthread_mutex_lock(&lock);
+        temp_x_real = x_real;
+        temp_y_real = y_real;
+        pthread_mutex_unlock(&lock);
 
         //COLISÃO
-        if (x_real <= temp_x_sprite + 21 &&      //esquerda 
-            x_real + 21 >= temp_x_sprite &&      //direita
-            y_real <= 224 + 22 &&           //cima
-            y_real + 22 >= 224) {           //baixo
+        if (temp_x_real <= x_sprite + 21 &&      //esquerda 
+            temp_x_real + 21 >= x_sprite &&      //direita
+            temp_y_real <= 224 + 22 &&           //baixo
+            temp_y_real + 22 >= 224) {           //cima
             
-            printf("COLISÃO\n");
-            colisao = 1;
+            pthread_mutex_lock(&lock);
+            parar=1;
+            pthread_mutex_unlock(&lock);
 
+            printf("COLIDIU\n");
             break;
         }
+
+        pthread_mutex_lock(&lock);
+        clicou2 = clicou;
+        pthread_mutex_unlock(&lock);
+
+        if(clicou2) break;
     }
-    
-    if(colisao==0) colisao = 1;
 
     //AGUARDA A FINALIZAÇÃO DE DETERMINADA THREAD
     if (pthread_join(thread_id, NULL) != 0) {
@@ -148,7 +164,7 @@ int main() {
         return 1;
     }
 
-    pthread_mutex_destroy(&x_sprite_mutex);
+    pthread_mutex_destroy(&lock);
 
     // Fechar os dispositivos
     close(fd_mouse);
