17d4a0d2b5610663d8e1133b1efddea7b2104563
Lucas Gabriel <142092072+LucaasGy@users.noreply.github.com>
2024-07-11 13:20:35 -0300

    feat: Atualizando posição dos policiais

--- utils.h
+++ utils.h
@@ -1,80 +1,69 @@
-#ifndef THREADS_H
-#define THREADS_H
+#ifndef UTILS_H
+#define UTILS_H
 
-#include "utils.h"
-#include <pthread.h>
+#define ESQ_SPRITE_LADRAO 5
+#define DIR_SPRITE_LADRAO 5
 
-#define MOUSE_DEVICE "/dev/input/event0"
+#define ESQ_SPRITE_POLICIA 3
+#define DIR_SPRITE_POLICIA 4
 
-#define SENTIDO_PARA_DIREITA 1
-#define SENTIDO_PARA_ESQUERDA 2
-#define SENTIDO_PARA_CIMA 3
-#define SENTIDO_PARA_BAIXO 4
+#define INICIO_POLICIAL_1_X 160 - ESQ_SPRITE_POLICIA
+#define INICIO_POLICIAL_2_X 600 + DIR_SPRITE_POLICIA
+#define INICIO_POLICIAL_3_X 456 - ESQ_SPRITE_POLICIA
+#define INICIO_POLICIAL_4_X 400 + DIR_SPRITE_POLICIA
+#define INICIO_POLICIAL_5_X 88 - ESQ_SPRITE_POLICIA
+#define INICIO_POLICIAL_6_X 608 + ESQ_SPRITE_POLICIA
+#define INICIO_POLICIAL_7_X 392
+#define INICIO_POLICIAL_8_X 96 - ESQ_SPRITE_POLICIA
+#define INICIO_POLICIAL_9_X 456 - ESQ_SPRITE_POLICIA
+#define INICIO_POLICIAL_10_X 208 + DIR_SPRITE_POLICIA
 
-#define VELOCIDADE_POLICIAIS_2_3_6_8 5000
-#define VELOCIDADE_POLICIAIS_4_5_7 3000
-#define VELOCIDADE_POLICIAIS_9_10 5000
-#define VELOCIDADE_POLICIAIS_1 2000
+#define FIM_POLICIAL_1_X 400 + DIR_SPRITE_POLICIA
+#define FIM_POLICIAL_2_X 456 - ESQ_SPRITE_POLICIA
+#define FIM_POLICIAL_3_X 608 + DIR_SPRITE_POLICIA
+#define FIM_POLICIAL_4_X 88 - ESQ_SPRITE_POLICIA
+#define FIM_POLICIAL_5_X 400 + DIR_SPRITE_POLICIA
+#define FIM_POLICIAL_6_X 456 - ESQ_SPRITE_POLICIA
+#define FIM_POLICIAL_7_X 96 - ESQ_SPRITE_POLICIA
+#define FIM_POLICIAL_8_X 208 + DIR_SPRITE_POLICIA
+#define FIM_POLICIAL_9_X 600 + DIR_SPRITE_POLICIA
+#define FIM_2_POLICIAL_9_X 392 - ESQ_SPRITE_POLICIA
+#define FIM_POLICIAL_10_X 24 - ESQ_SPRITE_POLICIA
 
-extern int fd_mouse;
+#define INICIO_POLICIAL_1_Y 8
+#define INICIO_FIM_POLICIAL_2_Y 72
+#define INICIO_POLICIAL_3_Y 128
+#define INICIO_FIM_POLICIAL_4_Y 160
+#define INICIO_FIM_POLICIAL_5_Y 216
+#define INICIO_FIM_POLICIAL_6_Y 296
+#define INICIO_POLICIAL_7_Y 336
+#define INICIO_FIM_POLICIAL_8_Y 384
+#define INICIO_POLICIAL_9_Y 368
+#define INICIO_POLICIAL_10_Y 448
 
-extern pthread_mutex_t lock;
+#define FIM_POLICIAL_1_Y 96
+#define FIM_POLICIAL_3_Y 184
+#define FIM_POLICIAL_7_Y 288
+#define FIM_POLICIAL_9_Y 448
+#define FIM_POLICIAL_10_Y 200
 
-extern pthread_t thread_mouse, thread_policais_1, thread_policiais_2_3_6_8, thread_policiais_4_5_7, thread_policias_9_10;
+#define INICIO_LADRAO_X 456
+#define INICIO_LADRAO_Y 32
 
-extern volatile int x_ladrao;
-extern volatile int y_ladrao;
+#define PORTA_X 456
+#define PORTA_Y 0
 
-extern volatile int policia_1_x;
-extern volatile int policia_2_x;
-extern volatile int policia_3_x;
-extern volatile int policia_4_x;
-extern volatile int policia_5_x;
-extern volatile int policia_6_x;
-extern volatile int policia_7_x;
-extern volatile int policia_8_x;
-extern volatile int policia_9_x;
-extern volatile int policia_10_x;
+#define TROFEU_ESQ_X 24
+#define TROFEU_ESQ_Y 16
 
-extern volatile int policia_1_y;
-extern volatile int policia_2_y;
-extern volatile int policia_3_y;
-extern volatile int policia_4_y;
-extern volatile int policia_5_y;
-extern volatile int policia_6_y;
-extern volatile int policia_7_y;
-extern volatile int policia_8_y;
-extern volatile int policia_9_y;
-extern volatile int policia_10_y;
+#define TROFEU_DIR_X 96
+#define TROFEU_DIR_Y 104
 
-extern volatile int cancela_threads_policiais;
-extern volatile int cancela_thread_botoes;
+#define PAUSE_X 608
+#define PAUSE_Y 0
 
-extern volatile int pausar;
-extern volatile int start;
-extern volatile int sair;
-extern volatile int furtivo;
+int verifica_colisao_parede(int x_ladrao, int y_ladrao, int verificar, char x_y);
 
-extern volatile int jogando;
-
-extern volatile int habilidades;
-
-int cria_threads_jogo();
-
-int espera_cancelamento_threads_policias();
-
-int cancela_thread_mouse();
-
-void* movimenta_mouse(void* arg);
-
-void* movimenta_policiais_2_3_6_8(void* arg);
-
-void* movimenta_policiais_4_5_7(void* arg);
-
-void* movimenta_policiais_9_10(void* arg);
-
-void* movimenta_policiais_1(void* arg);
-
-void* botao(void* arg);
+int verifica_colisao_policia(int x_ladrao, int y_ladrao, int policia_x, int policia_y);
 
 #endif
